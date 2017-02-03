d65063720249ecf50de8f6db339245c51f0e34ed
louis MICHEL <louismichel959@gmail.com>
2017-02-03 19:01:25 +0100

    commentaires et  selection de matériaux

--- P1RV_CLONE/Assets/scripts/FeatureManagerLink.cs
+++ P1RV_CLONE/Assets/scripts/FeatureManagerLink.cs
@@ -1,51 +1,55 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class FeatureManagerLink : MonoBehaviour {
-	public string FunctionName;
-	public GameObject FeatureM; 
-	public InteractionSwitch InterScript ; 
-	public Edit FeatureMScript;
-	public bool test;
-	// Use this for initialization
-	void Start () {
-		FeatureM = GameObject.Find ("FeaturesManager");
-		InterScript = FeatureM.GetComponent<InteractionSwitch> ();
-		FeatureMScript = FeatureM.GetComponent<Edit> ();
-		test = false; 
-	}
+// Permet d'effectuer le lien entre l'UI et le FeaturesManager qui gère les modes d'interaction et de manipulation
+public class FeatureManagerLink : MonoBehaviour
+{
+    public string FunctionName;
+    public GameObject FeatureM;
+    public InteractionSwitch InterScript;
+    public Edit FeatureMScript;
+    public bool test;
+    // Use this for initialization
+    void Start()
+    { // initialisation des variables, recherche dans la scène
+        FeatureM = GameObject.Find("FeaturesManager");
+        InterScript = FeatureM.GetComponent<InteractionSwitch>();
+        FeatureMScript = FeatureM.GetComponent<Edit>();
+        //test = false; 
+    }
 
-	public void Click()
-	{
-		switch (FunctionName) 
-		{
-		case "NaturalMove":
-			InterScript.Desactiver ();
-			break;
-		case "Scale": 
-			InterScript.Activer ();
-			FeatureMScript.IsScaling = true; 
-			FeatureMScript.IsRotating = false;
-			FeatureMScript.IsTranslating= false;
-			break;
-		case "Rotate":
-			InterScript.Activer ();
-			FeatureMScript.Rotate ();
-			test = true;
-			break;
-		case "Translate":
-			InterScript.Activer ();
-			FeatureMScript.IsScaling = false; 
-			FeatureMScript.IsRotating = false;
-			FeatureMScript.IsTranslating = true;
-			break;
-		default:
-			Debug.Log ("error");
-			break;
-		}
-	}
-	// Update is called once per frame
-	void Update () {
+    public void Click() // selon le bouton appuyé (Rotate,Edit,Scale) active les fonctions associées
+    {
+        switch (FunctionName)
+        {
+            case "NaturalMove":
+                InterScript.Desactiver();
+                break;
+            case "Scale":  // active dans le FeaturesManager le scaling à travers un booléen 
+                InterScript.Activer();
+                FeatureMScript.IsScaling = true; 
+                FeatureMScript.IsRotating = false;
+                FeatureMScript.IsTranslating = true;
+                break;
+            case "Rotate": // active la rotation (toujours des booléens)
+                InterScript.Activer();
+                FeatureMScript.Rotate();
+			//test = true;
+                break;
+            case "Translate":
+                InterScript.Activer();
+                FeatureMScript.IsScaling = false; 
+                FeatureMScript.IsRotating = false;
+                FeatureMScript.IsTranslating = true;
+                break;
+            default:
+                Debug.Log("Erreur, aucun bouton associé");
+                break;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
 	
-	}
+    }
 }
