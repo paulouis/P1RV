e469f7e102b06acbad2d984833f6c636410d0b34
louis MICHEL <louismichel959@gmail.com>
2017-02-11 15:33:31 +0100

    TEST DU MULTI

--- P1RV_CLONE/Assets/scripts/ZQSDMove.cs
+++ P1RV_CLONE/Assets/scripts/ZQSDMove.cs
@@ -1,30 +1,39 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.Networking;
 
-public class ZQSDMove : MonoBehaviour {
-	private float m_MovementXInputValue ;
-	private float m_MovementZInputValue; 
-	private float m_MovementYInputValue;
-	public float m_speed ;
-	public GameObject Camera;
-	// Use this for initialization
-	void Start () {
+public class ZQSDMove : NetworkBehaviour
+{
+    private float m_MovementXInputValue;
+    private float m_MovementZInputValue;
+    private float m_MovementYInputValue;
+    public float m_speed;
+    public GameObject Camera;
+    // Use this for initialization
+    void Start()
+    {
 	
-	}
-	void Move()
-	{
-		m_MovementXInputValue = Input.GetAxis ("Horizontal");
-		m_MovementZInputValue = Input.GetAxis ("Vertical");
-		m_MovementYInputValue = Input.GetAxis ("Hauteur");
-		Vector3 MovementForward = Camera.transform.forward * m_MovementZInputValue * m_speed * Time.deltaTime; 
-		Vector3 MovementDraft = Camera.transform.right * m_MovementXInputValue * m_speed * Time.deltaTime;
-		Vector3 MovementUp = transform.up * m_MovementYInputValue * m_speed * Time.deltaTime;
-		transform.Translate ( MovementDraft, Space.World);
-		transform.Translate (MovementForward);
-		transform.Translate (MovementUp);
-	}
-	// Update is called once per frame
-	void Update () {
-		Move ();
-	}
+    }
+
+    void Move()
+    {
+        m_MovementXInputValue = Input.GetAxis("Horizontal");
+        m_MovementZInputValue = Input.GetAxis("Vertical");
+        m_MovementYInputValue = Input.GetAxis("Hauteur");
+        Vector3 MovementForward = Camera.transform.forward * m_MovementZInputValue * m_speed * Time.deltaTime; 
+        Vector3 MovementDraft = Camera.transform.right * m_MovementXInputValue * m_speed * Time.deltaTime;
+        Vector3 MovementUp = transform.up * m_MovementYInputValue * m_speed * Time.deltaTime;
+        transform.Translate(MovementDraft, Space.World);
+        transform.Translate(MovementForward);
+        transform.Translate(MovementUp);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+        Move();
+    }
 }
