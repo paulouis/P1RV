084a3b4f168faa362be4b23a65977f7270665b59
louis MICHEL <louismichel959@gmail.com>
2017-02-11 16:51:36 +0100

    CA MARCHE ! (enfin un peu quoi)

---
+++ P1RV_CLONE/Assets/colorplayer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class colorplayer : NetworkBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+	
+    }
+
+    public override void OnStartLocalPlayer()
+    {
+        GetComponent<MeshRenderer>().material.color = Color.blue;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+    }
+}
